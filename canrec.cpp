d48d45266efeaa936e932f81559aa37bf149cf59
Your Name <you@example.com>
2023-11-13 15:57:35 +0100

    improved printing

--- canrec.cpp
+++ canrec.cpp
@@ -15,6 +15,12 @@ int canrec(int argc, char**argv)
 	int pass = 0;
 	int fail = 0;
 
+	time.tv_sec = 1;
+	time.tv_usec = 0;
+
+	int printFrame = 10;
+	int counter = 0;
+
     if(argc != 3)
     {
         printf("usage: canrec canbus\r\n");
@@ -22,7 +28,12 @@ int canrec(int argc, char**argv)
         return 0;
     }
 
-	can.Init(argv[2], errCode);
+	if(can.Init(argv[2], errCode))
+	{
+		printf("can init ok %s\r\n", argv[2]);
+	}
+
+	printf("printing every %d th frame\r\n", printFrame);
 
 	while(i < (RX_MESSAGES * 2))
 	{
@@ -46,11 +57,21 @@ int canrec(int argc, char**argv)
 			{
 				fail++;
 
+				printf("wrong frame received id:%d data:%d %d %d %d %d %d %d %d\r\n", msg.can_id,
+					msg.data[0], msg.data[1], msg.data[2], msg.data[3],
+					msg.data[4], msg.data[5], msg.data[6], msg.data[7]);
+
 				// resync i
 				i = msg.data[0] + (msg.data[1] << 8) + (msg.data[2] << 16) + (msg.data[3] << 24);
 				i+=2;
 			}
-			printf("%d %d\r\n", pass, fail);
+
+			if((counter % printFrame) == 0)
+			{
+				printf("%d %d\r\n", pass, fail);
+			}
+
+			counter++;
 		}
 	}
 	
